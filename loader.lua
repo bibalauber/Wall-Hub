9d79fbc2f8d40df126e154430f0f75fbff4446f0
bibalauber <129275765+bibalauber@users.noreply.github.com>
2024-08-31 03:45:43 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,8 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
 
+if game.GameId == 6035872082 then
+    Library:Notify("Loading Rivals Script.")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bibalauber/Wall-Hub/main/Rivals.lua"))()
+else
+    Library:Notify("There is no universal script right now :(")
+end
